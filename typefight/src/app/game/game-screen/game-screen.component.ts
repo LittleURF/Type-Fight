d8f97efcba4119d172ddcf0e6da8a99efb4f0df5
LittleURF <hubertzachlowski@gmail.com>
2021-07-20 21:27:02 +0200

    initial screen split config

--- typefight/src/app/game/game-screen/game-screen.component.ts
+++ typefight/src/app/game/game-screen/game-screen.component.ts
@@ -1,16 +1,16 @@
-import { GameScreenRouterService } from './../services/game-screen-router.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Screen } from '../models';
+
 
 @Component({
 	selector: 'app-game-screen',
 	templateUrl: './game-screen.component.html',
 	styleUrls: ['./game-screen.component.scss'],
+	providers: [{ provide: 'id', useFactory: asd }],
 })
-export class GameScreenComponent implements OnInit {
-	@Input() screen: Screen;
+// use factory to somehow determine what screen is it currently. fe. there are 2 screens in config. every time it gets returned it gets popped. that way 2 components can work.
+export class GameScreenComponent {
+	@Input() screene: Screen;
 
-	constructor(private screenRouter: GameScreenRouterService) {}
-
-	ngOnInit(): void {}
+	constructor() {}
 }
